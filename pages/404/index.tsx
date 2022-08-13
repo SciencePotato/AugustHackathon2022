969d8d5f6aa61e34046becbe3c58e0297ed3fd2e
Yu Zeng <zengy2003@gmail.com>
2022-08-13 11:43:06 -0700

    404 error

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -5,17 +5,17 @@ import scss from './../../styles/404.module.scss';
 
 function Error() {
     return (<> 
-    <p className={scss.error0}>
-    An error has occured.
-       <br></br>Try again.
-    </p>
     <p className={scss.errorOne}>
        <br></br> 404
     </p>
+    <p className={scss.errorZero}>
+    An error has occured.
+       <br></br>Try again.
+    </p>
     
-        <p className={scss.error}>
+        <p className={scss.errorThree}>
             <Link href={"/home"}> 
-                <button>
+                <button className="button">
                     Back to the Home Page
                 </button>
             </Link>
