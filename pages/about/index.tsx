9c5dc3cfdcab8ae2a57cc97488e837caeb8ce758
qizongliang <QIZONG.LIANG59@myhunter.cuny.edu>
2022-08-13 01:11:56 -0400

    firebase setup and .env file

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/about.module.scss';
 
+
+
 const About: NextPage = () => {
   return (
     <div className={styles.container}>
