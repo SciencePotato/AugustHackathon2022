5801b8801a39fc29d9a1cc994042ccec221572af
SciencePotato <houchi6mail@gmail.com>
2022-08-13 01:08:42 -0400

    footer Done

--- component/footer.tsx
+++ component/footer.tsx
@@ -1,10 +1,40 @@
 import type { NextPage } from 'next';
-import '../styles/footer.module.scss'
+import Link from 'next/link';
+import scss from '../styles/footer.module.scss'
 
-const Footer: NextPage = () => {
+const Footer: NextPage = (props) => {
   return (
-    <div>
-    </div>
+    <section className={scss.footer}>
+      <div>
+        <div> 
+          <h1>
+            Contact Us
+          </h1>
+        </div>
+      </div>
+      <div>
+          <h1>
+            <Link href={"https://github.com/qizongliang"}> 
+              @qizongliang: https://github.com/qizongliang
+            </Link>
+          </h1>
+          <h1>
+            <Link href={"https://zikunw.github.io"}> 
+              @zikunw: https://zikunw.github.io
+            </Link>
+          </h1>
+          <h1>
+            <Link href={"https://sciencepotato.github.io/personal-website/"}> 
+              @SciencePotato: https://github.com/SciencePotato
+            </Link>
+          </h1>
+          <h1>
+            <Link href={"https://github.com/YuZW42"}> 
+              @YuZW424: https://github.com/YuZW424
+            </Link>
+          </h1>
+      </div>
+    </section>
   )
 }
 
