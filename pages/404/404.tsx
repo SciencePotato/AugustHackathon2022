3be81007ab5693ca12efd03974f94d8f1e68bf41
Yu Zeng <zengy2003@gmail.com>
2022-08-13 00:13:09 -0700

    started on 404, needs style and button

---
+++ pages/404/404.tsx
@@ -0,0 +1,33 @@
+import { NextPage, NextPageContext} from 'next';
+
+import Link from 'next/link';
+import scss from './../../styles/404.module.scss';
+
+interface Error404Props{
+    statusCode?: number
+}
+
+const Error: NextPage<Error404Props> =({statusCode}) => {
+    return (
+        <p>
+            {statusCode ? 'An error ${statusCode} occurred'
+            : "An error occurred"}
+
+        
+        </p>
+        
+    );
+}
+
+Error.getInitialProps = ({res,err}: NextPageContext) => {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+    return {statusCode}
+}
+<div>
+    <Link href={"/home"}> 
+        <button>
+            Back to the Home Page
+        </button>
+    </Link>
+</div>
+export default Error
