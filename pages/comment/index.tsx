8a9042862c307c0404d58f9fa129d5e59ab049f6
SciencePotato <houchi6mail@gmail.com>
2022-08-13 20:43:57 -0400

    Fixed mistake

---
+++ pages/comment/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+import '../../styles/comment.module.scss'
+
+const Comment: NextPage = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Comment
